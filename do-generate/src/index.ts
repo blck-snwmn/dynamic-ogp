92d3b2e8201cfca30ae65cee36e83cd5ca28bcfd
blck-snwmn <whitesnowmancreator@gmail.com>
2024-01-03 16:58:25 +0900

    Add generate  worker

--- do-generate/src/index.ts
+++ do-generate/src/index.ts
@@ -1,5 +1,10 @@
-// biome-ignore lint/complexity/noBannedTypes: <explanation>
-export type Env = {};
+import { Buffer } from "node:buffer";
+import puppeteer from "@cloudflare/puppeteer";
+
+export type Env = {
+	MYBROWSER: puppeteer.BrowserWorker;
+	URL: string;
+};
 
 export default {
 	async fetch(
@@ -7,6 +12,23 @@ export default {
 		env: Env,
 		ctx: ExecutionContext,
 	): Promise<Response> {
-		return new Response("Hello World!");
+		const url = new URL(request.url);
+		if (url.pathname !== "/generate") {
+			return new Response("Not found", { status: 404 });
+		}
+
+		const browser = await puppeteer.launch(env.MYBROWSER);
+		const page = await browser.newPage();
+		await page.setViewport({ width: 300, height: 100 });
+
+		await page.goto(env.URL);
+
+		const img = (await page.screenshot()) as Buffer;
+		await browser.close();
+		return new Response(img, {
+			headers: {
+				"content-type": "image/jpeg",
+			},
+		});
 	},
 };
