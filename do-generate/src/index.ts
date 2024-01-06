e8e839f7e8da127d59751babb0774a0e1af34c3d
blck-snwmn <whitesnowmancreator@gmail.com>
2024-01-06 20:52:25 +0900

    Add Durable Object for browser and implement fetch method

--- do-generate/src/index.ts
+++ do-generate/src/index.ts
@@ -5,6 +5,7 @@ import puppeteer from "@cloudflare/puppeteer";
 type Bindings = {
 	MYBROWSER: puppeteer.BrowserWorker;
 	URL: string;
+	BROWSER: DurableObjectNamespace;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -24,36 +25,10 @@ app.get("/generate/:id", async (c) => {
 	const { id } = c.req.param();
 	console.log(`id: ${id}`);
 
+	const doid = c.env.BROWSER.idFromName("browser");
+	const obj = c.env.BROWSER.get(doid);
 
-	console.log(`before: ${new Date()}`);
-	const browser = await puppeteer.launch(c.env.MYBROWSER);
-
-	console.log(`after setup(launch): ${new Date()}`);
-
-	const page = await browser.newPage();
-
-	console.log(`after setup(new page): ${new Date()}`);
-
-	await page.setViewport({ width: 300, height: 100 });
-
-	console.log(`after setup(setViewport): ${new Date()}`);
-
-	await page.goto(`${c.env.URL}/${id}`);
-
-	console.log(`after goto: ${new Date()}`);
-
-	const img = (await page.screenshot()) as Buffer;
-
-	c.executionCtx.waitUntil(browser.close());
-
-	console.log(`end: ${new Date()}`);
-
-	const resp = new Response(img, {
-		headers: {
-			"content-type": "image/jpeg",
-			"cache-control": "public, s-maxag=120",
-		},
-	});
+	const resp = await obj.fetch(`${c.env.URL}/${id}`);
 
 	c.executionCtx.waitUntil(cache.put(cacheKey, resp.clone()));
 
@@ -61,3 +36,65 @@ app.get("/generate/:id", async (c) => {
 });
 
 export default app;
+
+const KEEP_BROWSER_ALIVE_IN_SECONDS = 60;
+const TEN_SECONDS = 1000 * 10;
+
+export class Browser {
+	state: DurableObjectState;
+	storage: DurableObjectStorage;
+	env: Bindings;
+	keepAliveInSec: number;
+	browser: puppeteer.Browser | undefined;
+	constructor(state: DurableObjectState, env: Bindings) {
+		this.state = state;
+		this.storage = this.state.storage;
+		this.env = env;
+		this.keepAliveInSec = 0;
+	}
+
+	async fetch(request: Request): Promise<Response> {
+		console.info("call fetch()");
+		if (!this.browser) {
+			// launch browser if it's not launched
+			console.info("launch browser");
+			this.browser = await puppeteer.launch(this.env.MYBROWSER);
+		}
+
+		this.keepAliveInSec = 0;
+
+		const page = await this.browser.newPage();
+		await page.setViewport({ width: 300, height: 100 });
+		await page.goto(request.url);
+		const img = (await page.screenshot()) as Buffer;
+
+		this.keepAliveInSec = 0;
+
+		const currentAlarm = this.storage.getAlarm();
+		if (currentAlarm === null) {
+			// set alarm if it's not set
+			console.info("set alarm in fetch()");
+			this.storage.setAlarm(Date.now() + TEN_SECONDS);
+		} else {
+			console.info("alarm is already set");
+		}
+
+		return new Response(img, {
+			headers: {
+				"content-type": "image/jpeg",
+				"cache-control": "public, s-maxag=120",
+			},
+		});
+	}
+
+	async alarm() {
+		this.keepAliveInSec += 10;
+		if (this.keepAliveInSec < KEEP_BROWSER_ALIVE_IN_SECONDS) {
+			// set alarm again if browser is alive
+			console.info(`set alarm again in alarm(): keepAliveInSec: ${this.keepAliveInSec}`);
+			this.storage.setAlarm(Date.now() + TEN_SECONDS);
+			return;
+		}
+		console.info("close browser");
+	}
+}
